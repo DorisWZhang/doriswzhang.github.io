e1576d9743f8dd82d3d7e7ee88ca40591c7ff259
Doris Zhang <doriszhang912@gmail.com>
2024-05-18 14:56:31 -0400

    changed paths

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,9 @@ function onResumeClick() {
 
 function Navbar() {
     const handleLinkClick = (id) => {
-        if (window.location.pathname !== "/doriswzhang.github.io") {
+        if (window.location.pathname !== "/") {
             // If not on home page, redirect to home page and scroll after it's loaded
-            window.location.href = "/doriswzhang.github.io";
+            window.location.href = "/";
             window.onload = () => {
             setTimeout(() => {
                 const element = document.getElementById(id);
@@ -43,7 +43,7 @@ function Navbar() {
 
 
     useEffect(() => {
-        if (window.location.pathname !== "/doriswzhang.github.io") {
+        if (window.location.pathname !== "/") {
             // If redirected from another page, scroll to the section after the home page is loaded
             const id = window.location.hash.substring(1);
             window.onload = () => {
@@ -59,7 +59,7 @@ function Navbar() {
         <div className="navbar" id={expandNavbar ? "open" : "close"}>
             <div className="links">
                 <a className="pages">
-                    <NavLink className="home-link" onClick={() => handleLinkClick("home")} to="/doriswzhang.github.io" > HOME</NavLink>
+                    <NavLink className="home-link" onClick={() => handleLinkClick("home")} to="/" > HOME</NavLink>
                     <NavLink className="about-link" onClick={() => handleLinkClick("about")} > ABOUT </NavLink>
                     <NavLink className="projects-link" onClick={() => handleLinkClick("projects")}  > PROJECTS </NavLink>
                     <NavLink className="resume-link" onClick={onResumeClick}> RESUME </NavLink>
